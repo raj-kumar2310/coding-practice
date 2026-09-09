f59ac012c4e27421484cb056b525a65ccacbecde
RAJKUMAR S <rajkumarsivakumar373@gmail.com>
2026-09-09 18:25:12 +0530

    Time: 1 ms (99.36%) | Memory: 42.6 MB (56.41%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long countCommas(long n) {
+        long c=1000;
+        long r=0;
+
+        while(c<=n){
+            r+=n-c+1;
+            c*=1000;
+        }
+
+        return r;
+
+    }
+}
