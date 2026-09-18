9f4e1d61b84bf295656a639e57dd0df40411d173
RAJKUMAR S <rajkumarsivakumar373@gmail.com>
2026-09-18 21:00:57 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (67.92%) - LeetSync

---
+++ 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+
+        if(n!=m)return false;
+
+        int[] a=new int[26];
+        // int[] b=new int[26];
+
+        for(int i=0;i<n;i++){
+            a[word1.charAt(i)-'a']++;
+            a[word2.charAt(i)-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(a[i] >3 || a[i] <-3)return false;
+        }
+
+        return true;
+    }
+}
